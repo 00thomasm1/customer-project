0760d1c50c95b8d062f886b10807d20b822b6d0d
00thomasm1 <116963@newporthigh.co.uk>
2017-03-30 11:04:13 +0100

    Add files via upload

---
+++ Customer.Java/Customer.Java/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class customer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtHolidayLocation = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(142, 12)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(114, 20)
+        Me.txtFirstName.TabIndex = 0
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Location = New System.Drawing.Point(142, 38)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(114, 20)
+        Me.txtSurname.TabIndex = 1
+        '
+        'txtHolidayLocation
+        '
+        Me.txtHolidayLocation.Location = New System.Drawing.Point(142, 64)
+        Me.txtHolidayLocation.Name = "txtHolidayLocation"
+        Me.txtHolidayLocation.Size = New System.Drawing.Size(114, 20)
+        Me.txtHolidayLocation.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "First name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Surname"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Email"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 64)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Holday location"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(142, 90)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(114, 20)
+        Me.txtEmail.TabIndex = 8
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(175, 116)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(81, 48)
+        Me.btnAdd.TabIndex = 9
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(175, 201)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(81, 48)
+        Me.btnSearch.TabIndex = 10
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(55, 216)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(114, 20)
+        Me.txtSearch.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(8, 219)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Search"
+        '
+        'customer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtHolidayLocation)
+        Me.Controls.Add(Me.txtSurname)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Name = "customer"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtSurname As System.Windows.Forms.TextBox
+    Friend WithEvents txtHolidayLocation As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Customer.Java/Customer.Java/Form1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.IO
+Public Class customer
+    Dim records(63) As CustomerInfo
+    Dim i As Integer
+    Private Structure CustomerInfo
+        Public CustomerFirstName As String
+        Public CustomerSurname As String
+        Public CustomerHolidayLocation As String
+        Public CustomerEmail As String              'creating a data structure to store the inputted data
+    End Structure
+
+    Private Sub customer_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        If Dir$("Customer.txt") = "" Then
+            Dim sw As New StreamWriter("Customer.txt", True)
+            sw.WriteLine("")
+            sw.Close()
+            MsgBox("A new file has been created", vbExclamation, "Warning!")
+        End If
+    End Sub
+
+    Private Sub btnAdd_Click(sender As System.Object, e As System.EventArgs) Handles btnAdd.Click
+        Dim CustomerData As New CustomerInfo
+        Dim sw As New System.IO.StreamWriter("customer.txt", True)
+        CustomerData.CustomerFirstName = LSet(txtFirstName.Text, 50)
+        CustomerData.CustomerSurname = LSet(txtSurname.Text, 50)
+        CustomerData.CustomerHolidayLocation = LSet(txtHolidayLocation.Text, 50)
+        CustomerData.CustomerEmail = LSet(txtEmail.Text, 50)
+    End Sub
+
+    Private Sub btnSearch_Click(sender As System.Object, e As System.EventArgs) Handles btnSearch.Click
+        Dim Search As String
+        Search = txtSearch.Text
+        For q = 0 To 63
+            If records(q).CustomerFirstName = Search Or records(q).CustomerSurname = Search Or records(q).CustomerHolidayLocation = Search Or records(q).CustomerEmail = Search Then
+                MessageBox.Show("records" & vbCrLf & _  "the customers name is" & records(q).CustomerFirstName  & records(q).CustomerSurname  &)
+            End If
+        Next Then
+    End Sub
+End Class ' not fininshed
